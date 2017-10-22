86f1fe3dd0a663e68b34467d8df9426377a099f5
Mical Juarez <mical.juarez@gmail.com>
2017-10-21 23:55:59 -0500

    added constructor for World and function for looping to create string map

--- ch7-project-elements-of-life/ch7-elements-of-life.js
+++ ch7-project-elements-of-life/ch7-elements-of-life.js
@@ -36,3 +36,60 @@ Grid.prototype.set = function(vector, value) {
 //
 // grid.set(new Vector(1, 1), "X");
 // console.log(grid.get(new Vector(1, 1)));
+
+
+// directions keys for the moving critters
+var directions = {
+  "n": new Vector(0, -1),
+  "ne": new Vector(1, -1),
+  "e": new Vector(1, 0),
+  "se": new Vector(1, 1),
+  "s": new Vector(0, 1),
+  "sw": new Vector(-1, 1),
+  "w": new Vector(-1, 0),
+  "nw": new Vector(-1, -1)
+}
+
+// helper function that chooses random element of an array
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// array for random directions to be chosen from
+var directionNames = "n ne e se s sw w nw".split(" ");
+
+// simple bouncing critter that moves randomly
+function BouncingCritter() {
+  this.direction = randomElement(directionNames);
+}
+
+// act method that allows the critter to view in a direction
+BouncingCritter.prototype.act = function(view) {
+  if (view.look(this.direction) != " ") {
+    this.direction - view.find(" ") || "s";
+  }
+  return {type: "move", direction: this.direction};
+}
+
+// ??????????????????????????????????????????????????????
+function elementFromChar(legend, ch) {
+  if (ch == " ") {
+    return null;
+  }
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+// world constructor to create world based on the map, using the legend
+function World(map, legend) {
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function(line, y) {
+    for (var i = 0; i < line.length; i++) {
+      grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+    }
+  });
+}
