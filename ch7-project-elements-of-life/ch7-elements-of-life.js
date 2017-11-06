493f94487174c28fcdbfd6c878de19dfef04e5d8
Mical Juarez <mical.juarez@gmail.com>
2017-11-06 00:01:36 -0600

    added view object to display corresponding elements, and can now show movement in a basic set of screenshots

--- ch7-project-elements-of-life/ch7-elements-of-life.js
+++ ch7-project-elements-of-life/ch7-elements-of-life.js
@@ -42,6 +42,17 @@ Grid.prototype.set = function(vector, value) {
   this.space[vector.x + this.width * vector.y] = value;
 }
 
+Grid.prototype.forEach = function(f, context) {
+  for (var y = 0; y < this.height; y++) {
+    for (var x = 0; x < this.width; x++) {
+      var value = this.space[x + y * this.width];
+      if (value != null) {
+        f.call(context, value, new Vector(x, y));
+      }
+    }
+  }
+};
+
 // test of Grid prototype and its methods
 
 // var grid = new Grid(5, 5);
@@ -132,4 +143,72 @@ function Wall() {}
 
 var world = new World(plan, {"#": Wall, "o": BouncingCritter})
 
-console.log(world.toString());
+World.prototype.turn = function() {
+  var acted = [];
+  this.grid.forEach(function(critter, vector) {
+    if(critter.act && acted.indexOf(critter) == -1) {
+      acted.push(critter);
+      this.letAct(critter, vector);
+    }
+  }, this);
+};
+
+World.prototype.letAct = function(critter, vector) {
+  var action = critter.act(new View(this, vector));
+  if (action && action.type == "move") {
+    var dest = this.checkDestination(action, vector);
+    if (dest && this.grid.get(dest) == null) {
+      this.grid.set(vector, null);
+      this.grid.set(dest, critter);
+    }
+  }
+};
+
+World.prototype.checkDestination = function(action, vector) {
+  if (directions.hasOwnProperty(action.direction)) {
+    var dest = vector.plus(directions[action.direction]);
+    if (this.grid.isInside(dest)) {
+      return dest;
+    }
+  }
+};
+
+
+// View object to find if selected coordinates are on the grid and returns the corresponding element
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+
+View.prototype.look = function(dir) {
+  var target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target)) {
+    return charFromElement(this.world.grid.get(target));
+  }
+  else {
+    return "#";
+  }
+};
+
+View.prototype.findAll = function(ch) {
+  var found = [];
+  for (var dir in directions) {
+    if (this.look(dir) == ch) {
+      found.push(dir);
+    }
+  }
+  return found;
+};
+
+View.prototype.find = function(ch) {
+  var found = this.findAll(ch);
+  if (found.length == 0) {
+    return null;
+  }
+  return randomElement(found);
+}
+
+for (var i = 0; i < 5; i++) {
+  world.turn();
+  console.log(world.toString());
+}
