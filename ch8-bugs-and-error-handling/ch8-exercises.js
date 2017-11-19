7af7fb07d1155a77cf87b0c0e08f95440a41ae28
Mical Juarez <mical.juarez@gmail.com>
2017-11-18 23:38:10 -0600

    notes and examples for debugging, try/catch

---
+++ ch8-bugs-and-error-handling/ch8-exercises.js
@@ -0,0 +1,53 @@
+// simple testing example
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+// program to check that impelmentation of Vector works as intended
+function testVector() {
+  var p1 = new Vector(10, 20);
+  var p2 = new Vector(-10, 5);
+  var p3 = p1.plus(p2);
+
+  if (p1.x !== 10) return "fail: x property";
+  if (p1.y !== 20) return "fail: y property";
+  if (p2.x !== -10) return "fail: negative x property";
+  if (p3.x !== 0) return "fail: x from plus";
+  if (p3.y !== 25) return "fail: y from plus";
+  return "everything ok";
+};
+
+console.log(testVector());
+
+
+// example of throwing an error
+function promptDirection(question) {
+  var result = prompt(question, "");
+  if (result.toLowerCase() == "left") {
+    return "L";
+  }
+  if (result.toLowerCase() == "right") {
+    return "R";
+  }
+  throw new Error("Invalid Direction: " + result);
+};
+
+function look() {
+  if (promptDirection("Which way?") == "L") {
+    return "a house";
+  }
+  else {
+    return "two angry bears";
+  }
+};
+
+try {
+  console.log("You see", look());
+} catch(error) {
+  console.log("Something went wrong", error);
+};
