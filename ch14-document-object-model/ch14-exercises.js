48c9318543e09e5c431932a8c4218db14aec4fc3
Mical Juarez <mical.juarez@gmail.com>
2019-11-07 12:38:44 -0600

    just adding notes and small code to demonstrate ideas

---
+++ ch14-document-object-model/ch14-exercises.js
@@ -0,0 +1,18 @@
+// recursive function that will loop through a node to look for a given string
+// basically a rough ctrl + f feature
+function talksAbout(node, string) {
+  if (node.nodeType == Node.ELEMENT_NODE) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      if (talksAbout(node.childNodes[i], string)) {
+        return true;
+      }
+    }
+    return false;
+  } else if (node.nodeType == Node.TEXT_NODE) {
+    return node.nodeValue.indexOf(string) > -1;
+  }
+}
+
+console.log(talksAbout(document.body, "star wars"));
+
+console.log(document.createElement("div").innerHTML = "I was created using the createElement method.")
